dea8e8d097fc88d010ede108f2c51f0163e74e51
Cretis <57385253+Cretis@users.noreply.github.com>
2020-02-18 16:31:34 -0500

    Add files via upload

--- HW04a.py
+++ HW04a.py
@@ -6,16 +6,16 @@ def get_git_hub(a):
     rep = []
     result = []
     i = a
-    url1 = f"https://api.github.com/users/{i}/repos"
+    url1 = f'https://api.github.com/users/' + i + '/repos'
     response = requests.get(url1)
     answer = response.json()
     for r in answer:
         rep.append(r['name'])
     for k in rep:
-        url2 = f"https://api.github.com/repos/{i}/{k}/commits"
+        url2 = f'https://api.github.com/repos/' + i + '/' + k + '/commits'
         res = requests.get(url2)
         asn = res.json()
-        result.append(f"Repo:{k} Number of commits: {len(asn)}")
+        result.append(f'Repo:' + k + ' Number of commits: ' + str(len(asn)))
     return result
 
 
