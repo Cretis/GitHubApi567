26816257c37769a7a3ca5b11b1a397c90ab5e779
Cretis <57385253+Cretis@users.noreply.github.com>
2020-02-18 15:25:45 -0500

    Add files via upload

--- TestHW04a.py
+++ TestHW04a.py
@@ -4,10 +4,10 @@ from HW04a import get_git_hub
 
 class TestHw04(unittest.TestCase):
     def testCase1(self):
-        self.assertEqual(get_git_hub('Cretis'), ['Repo:GitHubApi567 Number of commits: 3',
+        self.assertEqual(get_git_hub('Cretis'), ['Repo:GitHubApi567 Number of commits: 5',
                                                  'Repo:SSW567 Number of commits: 2',
                                                  'Repo:SSW810 Number of commits: 4',
-                                                 'Repo:Triangle567 Number of commits: 17', ])
+                                                 'Repo:Triangle567 Number of commits: 17'])
 
 
 if __name__ == '__main__':
