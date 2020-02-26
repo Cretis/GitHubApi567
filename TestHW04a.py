b4b2d00376b99b592fe1bb5f141c0407103e2b2e
Cretis <57385253+Cretis@users.noreply.github.com>
2020-02-25 21:55:57 -0500

    Add files via upload

--- TestHW04a.py
+++ TestHW04a.py
@@ -1,13 +1,21 @@
 import unittest
+from unittest.mock import patch, Mock
+
 from HW04a import get_git_hub
 
 
 class TestHw04(unittest.TestCase):
     def testCase1(self):
-        self.assertEqual(get_git_hub('Cretis'), ['Repo:GitHubApi567 Number of commits: 15',
-                                                 'Repo:SSW567 Number of commits: 2',
-                                                 'Repo:SSW810 Number of commits: 4',
-                                                 'Repo:Triangle567 Number of commits: 17'])
+        with patch('HW04a.requests.get') as mock_get:
+            mock_get.return_value.ok = True
+            mock_get.return_value.json.return_value = [
+                {'name': '1'},
+                {'name': '2'}
+            ]
+            mocked = get_git_hub('Cretis')
+            self.assertEqual(mocked, ['Repo:1 Number of commits: 2',
+                                      'Repo:2 Number of commits: 2',
+                                      ])
 
 
 if __name__ == '__main__':
